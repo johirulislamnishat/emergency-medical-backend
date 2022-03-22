738c3c880fdd4303248bfa775d179a075324bb95
Johirul Islam Nishat <jahirulislamn@gmail.com>
2022-03-22 07:26:22 +0600

    appointment

--- index.js
+++ index.js
@@ -158,6 +158,23 @@ client.connect((err) => {
     res.json(result);
   });
 
+  //DOCTORS Single Lab
+  app.get("/appointments/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await appointmentCollection.findOne(query);
+    res.send(result);
+  });
+
+  //DELETE LAB API
+  app.delete("/appointments/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await appointmentCollection.deleteOne(query);
+    // console.log(result);
+    res.send(result);
+  });
+
   //BLOGS POST API
   app.post("/blogs", async (req, res) => {
     const blogs = req.body;
